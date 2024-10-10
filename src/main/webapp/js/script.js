63ac77c5b1300e035cc6c63cc997f5f80de399a2
Mario <mario.mai@stud.uni-due.de>
2024-10-10 21:13:23 +0200

    Kategorie Button mit Funktion implementiert
    Text wird abgefangen und modifiziert.

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -8,3 +8,47 @@ tinymce.init({
     content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }'
 });
 
+    function toggleKategorieForm() {
+        let eingabeBox = document.querySelector(".erstelleKategorieForm");
+        eingabeBox.style.display = eingabeBox.style.display === "block" ? "none" : "block";
+    }
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById('kategorieForm').addEventListener('submit', e => {
+        e.preventDefault();
+        const data = new FormData(e.target);
+        const bez = data.get('bezeichnung');
+        const tag = data.get('tag');
+        const farbe = data.get('farbe');
+
+        console.log(bez, tag, farbe);
+
+        document.getElementById('bezeichnung_output').innerHTML += `<p>[${bez}]</p>`;
+        document.getElementById('tag_output').innerHTML += `<p>[${tag}]</p>`;
+        document.getElementById('farbe_output').innerHTML += `<p><span style="background:${farbe}">[${farbe}]</span></p>`;
+        document.getElementById('kategorieForm').reset();
+        buttonErstellenKategorie(bez, tag, farbe);
+    })
+})
+
+function buttonErstellenKategorie(bez, tag, farbe) {
+    let btn = document.createElement("BUTTON");
+    let textNode = document.createTextNode(bez);
+    btn.appendChild(textNode);
+    document.getElementById("buttonleiste").appendChild(btn);
+
+    btn.addEventListener('click', function() {
+        const editor = tinymce.get('textarea'); //Instanziieren tinymce
+
+        let text = editor.selection.getContent();
+        if(text){
+            console.log('Markierter Text: ' + text)
+            //tinymce.activeEditor.insertContent(`<mark style="background:${farbe}!important">[${tag}]${text}]</mark>&nbsp`);
+            //&nbsp -> verhindert, dass nachfolgender Text mit markiert wird
+            tinymce.activeEditor.insertContent(`<span style="background:${farbe}!important">[${tag}]${text}]</span>&nbsp`);
+        }else{
+            alert('Es kann nicht, nichts markiert werden.');
+        }
+    });
+}
+
--- src/main/webapp/lehrendView.jsp
+++ src/main/webapp/lehrendView.jsp
@@ -11,12 +11,35 @@
 <head>
     <script type="module" src="js/script.js"></script>
     <script type="text/javascript" src="https://cdn.tiny.cloud/1/e0q0w4t1ep5f35kyxt999mzlm4ro2px179u580taztflq9sf/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
+    <link rel="stylesheet" href="css/style.css">
     <title>Aufagbe</title>
 </head>
 <body>
 <textarea id="textarea"></textarea>
-<h1>Text</h1>
+<div id="container">
+    <div id="buttonleiste"></div>
+    <div class="attribute-kategorie">
+        <label for="bezeichnung">Bezeichnung</label>
+        <label for="tag">Tag</label>
+        <label for="farbe">Farbe</label>
+        <!--<button id="btn-kategorie" onclick="toggleKategorieForm()">Erstelle Kategorie</button> -->
+    </div>
 
+    <div class="output-values">
+        <p id="bezeichnung_output"></p>
+        <p id="tag_output"></p>
+        <p id="farbe_output"></p>
+        <p id="leer"></p>
+    </div>
 
+    <div class="erstelleKategorieForm">
+        <form id="kategorieForm" name="kategorieForm">
+            <input id="bezeichnung" type="text" placeholder="Bezeichnung" name="bezeichnung" required>
+            <input id="tag" type="text" placeholder="Tag" name="tag" required>
+            <input id="farbe" type="text" placeholder="Color English" name="farbe" required>
+            <button type="submit">Hinzufügen</button>
+        </form>
+    </div>
+</div>
 </body>
 </html>
